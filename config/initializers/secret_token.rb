9517b09d19119dba8224ca2441836c5465ca1379
Michael Hitchens <michael_hitchens@mail.ru>
2015-02-25 13:10:37 +0300

    add project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApiMed::Application.config.secret_token = '60b1be35c100169ca6624694958c6afe32ec67239373aa4dba9b6dceb3f491c8d7aceb83c832dc8b25c830926ff390f713764f412b1081eea752444ff5612c9b'
